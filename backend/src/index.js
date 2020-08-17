24cc924f5796f84b2c47e190cc75d45311a13eac
theLazyProgrammer <electro_nik_54@hotmail.com>
2020-08-17 02:50:13 -0400

    setup server, db config file & mutation files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
 // let's go!
+require('dotenv').config({ path: 'variables.env' });
+
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+
+
+server.start(
+    {
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL
+        }
+    },
+    deets => {
+        console.log('%s\x1b[31m%s\x1b[0m%s\x1b[31m%s\x1b[0m',
+            '\nServer running @', ` http://localhost:${deets.port} `, '& playground @', ` http://localhost:${process.env.PORT} `);
+        console.log(`${new Date()}`)
+    }
+);
